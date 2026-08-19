3eea4484e8beccf28dac2f596a4a973c78c67651
Nicolas Guichard <nicolas.guichard@kdab.com>
2026-08-19 15:49:30 +0200

    Bug 1740290 - Cross-link scip-typescript and js-analyze symbols.
    scip-typescript won't entirely replace js-analyze, it can't index
    inline JavaScript in HTML and many other plain JavaScript files such as
    web platform tests.
    
    This tries to establish a bidirectional connection between js-analyze
    and scip-typescript symbols by taing inspiration from the js-analyze
    and IDL support:
    - when emitting the AnalysisStructured of a scip-typescript symbol, we
      add a js_sym member which holds the expected js-analyze symbol
    - then crossref takes those to create the reverse link in js-analyze
      symbols, just like it already does for IDL links
    - those are forwarded to jumpref
    - the context menu builds a “Possible TypeScript definitions” submenu
      just like it does for “Possible IDL definitions”
    
    For the other way around, router.py traverses the new crossref field
    `AnalysisStructured::js_sym` to add potential js-analyze symbol usage
    to scip-typescript symbol results. They are added to a new “Possible
    JavaScript Uses” section.

--- tools/src/file_format/jumpref.rs
+++ tools/src/file_format/jumpref.rs
@@ -41,6 +41,9 @@ pub struct JumprefData {
     pub jumps: Jumps,
     #[serde(default, skip_serializing_if = "Option::is_none")]
     pub idl_syms: Option<Vec<Ustr>>,
+    /// Candidate scip-typescript symbols if this is a js-analyze symbol
+    #[serde(default, skip_serializing_if = "Vec::is_empty")]
+    pub ts_syms: Vec<Ustr>,
 }
 
 /// Transform a crossref data that will be written into crossref into the
@@ -66,6 +69,7 @@ pub fn convert_crossref_value_to_sym_info_rep(
             },
             meta: None,
             idl_syms: None,
+            ts_syms: vec![],
         };
     };
 
@@ -98,6 +102,7 @@ pub fn convert_crossref_value_to_sym_info_rep(
         jumps,
         meta: cross_val.meta,
         idl_syms: cross_val.idl_syms,
+        ts_syms: cross_val.ts_syms,
     }
 }
 
@@ -134,6 +139,13 @@ pub fn determine_desired_extra_syms_from_jumpref(
             extra_syms.push((*sym, JumprefTraversals::NormalExtra));
         }
     }
+    extra_syms.extend(
+        jumpref
+            .ts_syms
+            .iter()
+            .copied()
+            .map(|sym| (sym, JumprefTraversals::NormalExtra)),
+    );
     if let Some(owner) = jumpref
         .meta
         .as_ref()
